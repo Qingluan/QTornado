0feb97d4c64dd5ae01ff08207abc9afaffe2c9bf
dark.H <darkhackdevil@gmail.com>
2015-02-05 02:42:49 +0800

    store bug

--- src/Qtornado.py
+++ src/Qtornado.py
@@ -110,6 +110,7 @@ class TreeFile(FillContentHandler):
 
         setting_file = os.path.join(cal_path,"setting.py")
         xmlTag = XmlTag(setting_file)
+        handler_name = handler_name[0].upper() + handler_name[1:]
         xmlTag.changeTag("route", "(r'/{}',{}),".format(path_name,handler_name))
 
         viewname=path_name + ".html"
